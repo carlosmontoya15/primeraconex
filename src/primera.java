37ec902f661897607ca75c97ff7df55e3beb14d8
emili <emili@DESKTOP-GILPLCP>
2023-04-19 13:26:11 +0200

    primera conexión

---
+++ src/primera.java
@@ -0,0 +1,9 @@
+
+public class primera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("al subir la primera vez en una sesión de eclipse");
+	}
+
+}
