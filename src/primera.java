91490f4ac3a49f879bf38115bf2592fc21e3957c
emili <emili@DESKTOP-GILPLCP>
2023-04-19 13:29:28 +0200

    primera vez

--- src/primera.java
+++ src/primera.java
@@ -3,7 +3,7 @@ public class primera {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("al subir la primera vez en una sesión de eclipse");
+		System.out.println("subir la primera vez en una sesión de eclipse");
 	}
 
 }
