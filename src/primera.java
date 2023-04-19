6f15cd0373a1eb9279e529391ab6f01cb89ede3d
emili <emili@DESKTOP-GILPLCP>
2023-04-19 16:20:53 +0200

    otras veces

--- src/primera.java
+++ src/primera.java
@@ -3,7 +3,7 @@ public class primera {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Subir  otras veces en una sesión de eclipse");
+		System.out.println("Subir otras veces en una sesión de eclipse");
 	}
 
 }
